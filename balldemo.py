1017cf827d6035bce1be7213ba82577b133cf03d
J. David Giese <johndgiese@gmail.com>
2020-07-22 16:28:41 -0400

    Add argument for the number of balls

--- balldemo.py
+++ balldemo.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import time
+import argparse
 
 import numpy as np
 import cv2
@@ -11,31 +12,35 @@ from gridmodel import GridModel
 from frameratechecker import FramerateChecker
 
 
-handleTensorflowSession(memoryLimit=0.2)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--balls", type=int, default=3)
+    args = parser.parse_args()
+
+    handleTensorflowSession(memoryLimit=0.2)
+
+    gridModel = GridModel("../grid_models/grid_model_submovavg_64x64.h5", args.balls)
+    cap = cv2.VideoCapture(0)
+    framerateChecker = FramerateChecker(expected_fps=30)
 
 
-gridModel = GridModel("../grid_models/grid_model_submovavg_64x64.h5")
-cap = cv2.VideoCapture(0)
-framerateChecker = FramerateChecker(expected_fps=30)
+    while True:
+        framerateChecker.check()
+        ret, original_img = cap.read()
+        if not ret:
+            print("Couldn't get frame from camera.")
+            break
+        else:
+            height, width, channels = original_img.shape
+            tocrop = int((width - height) / 2)
+            original_img = original_img[:,tocrop:-tocrop]
+            ballsAndHands = gridModel.predict(original_img.copy())
 
+            img = cv2.resize(original_img, (256,256), cv2.INTER_CUBIC)
+            drawBallsAndHands(img, ballsAndHands)
 
-while True:
-    framerateChecker.check()
-    ret, original_img = cap.read()
-    if not ret:
-        print("Couldn't get frame from camera.")
-        break
-    else:
-        height, width, channels = original_img.shape
-        tocrop = int((width - height) / 2)
-        original_img = original_img[:,tocrop:-tocrop]
-        ballsAndHands = gridModel.predict(original_img.copy())
+            cv2.imshow('Webcam', img)
+            cv2.waitKey(1)
 
-        img = cv2.resize(original_img, (256,256), cv2.INTER_CUBIC)
-        drawBallsAndHands(img, ballsAndHands)
-
-        cv2.imshow('Webcam', img)
-        cv2.waitKey(1)
-
-cap.release()
-cv2.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
